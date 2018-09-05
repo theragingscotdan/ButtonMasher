dd5deae8e5db5ca3256c811f34da7952262261a9
theragingscotdan <43002238+theragingscotdan@users.noreply.github.com>
2018-09-05 15:38:17 +0100

    added variable

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+// included libraries
+#include <SFML/Graphics.hpp>
+
+
+// entry point for the program
+int main()
+{
+	// make a variable called gameWindow of type RenderWindow
+	sf::RenderWindow gameWindow;
+
+	// game loop
+	while (true)
+	{
+		// TODO: Check for input
+
+		// TODO: Update game state
+
+		// TODO: Draw Graphics
+
+	}
+	// exit point for the program
+	return 0;
+}
